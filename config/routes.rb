85502ba9cb2280ce406a6207c295f7db6df9e5ef
kengo999 <iceche9@gmail.com>
2019-08-27 09:41:57 +0000

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  
+
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
